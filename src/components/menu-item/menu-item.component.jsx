e4ef0233eca6974d1600b7ab105dcd7730a411e1
Tushar <tm2582001@gmail.com>
2022-01-24 20:48:45 +0530

    updated our menu-items to navigate with router useNavigation

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,19 @@
 import "./menu-item.styles.scss";
+import { useNavigate } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  let nevigate = useNavigate();
+  return(
+    <div className={`${size} menu-item`} onClick={()=>nevigate(linkUrl)}>
     <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}  />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
-);
+  )
+  
+};
 
 export default MenuItem;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,7 @@
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
+// import { useNavigate } from 'react-router-dom';
+
 
 const HomePage = () =>(
     <div className="homepage">
